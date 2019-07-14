faca7fedb0265e27adf4e5cc7cc4b85a83a1c2d6
Joko Sarjono Slameto <johno.smakaduta@gmail.com>
2019-07-14 15:44:03 +0700

    Coba-coba Vue.js

---
+++ onlinestore-panel/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Tentang Saya</h1>
+  </div>
+</template>
---
+++ onlinestore-panel/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+      <!-- <div v-for="product in products" :key="product.id">
+      <h4>{{product.name}}</h4> -->
+      <div v-for="flow in flowers" :key="flow.id">
+      <h4>{{ flow.volumeInfo.title }}</h4>
+     <img
+       :src="flow.volumeInfo.imageLinks.thumbnail"
+       :alt="flow.volumeInfo.title" />
+   </div>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: brown;
+  size: 20px;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+</style>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+export default {
+  name: "home",
+   data() {
+    return {
+      // products: null
+      flowers: null
+    }
+  },
+  async mounted() {
+    // await axios
+      // .get('http//localhost:12345/products')
+      // .then(response => this.products = response.data)
+       await axios.get('https://www.googleapis.com/books/v1/volumes?q=flowers')
+     .then(response => (this.flowers = response.data.items ))
+  }
+ 
+};
+</script>
